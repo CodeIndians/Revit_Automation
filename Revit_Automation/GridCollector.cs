3579324add2a0adb229f3efc6b9f0db31d2a2a2e
Raghuveer Ch S <raghuveerchs@gmail.com>
2023-04-29 18:17:27 +0530

    RA-3: Main Grids equidistant validation
    Code restructuring

---
+++ Revit_Automation/GridCollector.cs
@@ -0,0 +1,109 @@
+﻿using Autodesk.Revit.Creation;
+using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Document = Autodesk.Revit.DB.Document;
+
+namespace Revit_Automation
+{
+    internal class GridCollector
+    {
+        private readonly Document mDocument;
+        private ICollection<Element> mGridCollection;
+        private List<Tuple<XYZ, XYZ>> mHorizontalLines;
+        private List<Tuple<XYZ, XYZ>> mVerticalLines;
+
+        public ICollection<Element> GridCollection { get { return mGridCollection; } }
+        public List<Tuple<XYZ, XYZ>> HorizontalLines { get { return mHorizontalLines; } }
+        public List<Tuple<XYZ, XYZ>> VerticalLines { get { return mVerticalLines; } }
+
+
+        public GridCollector(Document doc) 
+        {
+            mDocument = doc;
+            Initialize();
+        }
+        private void Initialize()
+        {
+            FilteredElementCollector gridCollector = new FilteredElementCollector(mDocument);
+            mGridCollection = gridCollector.OfCategory(BuiltInCategory.OST_Grids).ToElements();
+
+            List<Tuple<XYZ, XYZ>> gridLines = new List<Tuple<XYZ, XYZ>>();
+
+            // collect each line into a gridline tuple 
+            foreach (Element element in mGridCollection)
+            {
+                Parameter archParameter = element.LookupParameter("Arch Grid");
+
+                if(archParameter != null)
+                {
+                    int value = archParameter.AsInteger();
+                    // Skip if Arch Grid is checkmarked
+                    if (value == 1)
+                        continue;
+                }
+
+                Parameter mainGridParameter = element.LookupParameter("Main Grid");
+
+                if (mainGridParameter != null)
+                {
+                    int value = mainGridParameter.AsInteger();
+                    // Skip if Main Grid is not checkmarked
+                    if (value == 0)
+                        continue;
+                }
+
+                // add the tuple grid lines
+                Grid grid = element as Grid;
+                if (grid != null)
+                {
+                    var pair = Tuple.Create(grid.Curve.GetEndPoint(0), grid.Curve.GetEndPoint(1));
+                    gridLines.Add(pair);
+                }
+            }
+
+            double precision = 0.0001;
+
+            // Collect Horizontal and Vertical Lines
+            mHorizontalLines = gridLines.Where(pair => Math.Abs(pair.Item1.Y - pair.Item2.Y) < precision).ToList().OrderBy(pair => pair.Item1.Y).ToList();
+            mVerticalLines = gridLines.Where(pair => Math.Abs(pair.Item1.X - pair.Item2.X) < precision).ToList().OrderBy(pair => pair.Item1.X).ToList();
+        }
+
+        public bool Validate()
+        {
+
+            double precision = 0.0001;
+            bool isEquidistant = true;
+
+            // Check consecutive horizontal lines
+            for (int i = 0; i < mHorizontalLines.Count - 1; i++)
+            {
+                double distance = mHorizontalLines[i + 1].Item1.X - mHorizontalLines[i].Item1.X;
+                if (Math.Abs(distance - (mHorizontalLines[i].Item2 - mHorizontalLines[i].Item1).GetLength()) > precision)
+                {
+                    isEquidistant = false;
+                    break;
+                }
+            }
+
+            if (isEquidistant)
+            {
+                // Check consecutive vertical lines
+                for (int i = 0; i < mVerticalLines.Count - 1; i++)
+                {
+                    double distance = mVerticalLines[i + 1].Item1.Y - mVerticalLines[i].Item1.Y;
+                    if (Math.Abs(distance - (mVerticalLines[i].Item2 - mVerticalLines[i].Item1).GetLength()) > precision)
+                    {
+                        isEquidistant = false;
+                        break;
+                    }
+                }
+            }
+
+            return isEquidistant;
+        }
+    }
+}
---
+++ Revit_Automation/SymbolCollector.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_Automation
+{
+    internal class SymbolCollector
+    {
+        static public FamilySymbol T62columnType = null;
+        static public FamilySymbol T62FlushTopFlushBottom = null;
+        static public FamilySymbol T62FlushBottomFemaletop = null;
+        static public FamilySymbol T62FemaleTopMaleBottom = null;
+        static public FamilySymbol T62FlushTopMaleBottom = null;
+
+        static public void CollectColumns(Document doc)
+        {
+            string filePath = "C:\\temp\\example.txt"; // Path to the file to be created
+
+
+            FamilySymbol StudColumnType = null;
+            FilteredElementCollector coll = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_StructuralColumns);
+
+            foreach (FamilySymbol symbol in coll)
+            {
+                if (symbol.FamilyName == "T62")
+                {
+                    if (symbol.Name == "4\" x 4\" x 2\" (Flush Bottom / Female Top)")
+                        T62FlushBottomFemaletop = symbol;
+
+                    if (symbol.Name == "4\" x 4\" x 2 1/2\" (Male Bottom / Female Top)")
+                        T62FemaleTopMaleBottom = symbol;
+
+                    if (symbol.Name == "4\" x 4\" x 2\" (Male Bottom / Flush Top)")
+
+                        using (StreamWriter writer = new StreamWriter(filePath, true))
+                        {
+                            writer.WriteLine(symbol.Name);
+                            writer.Close();
+                        }
+                }
+
+                if (symbol.FamilyName == "Post")
+                {
+                    StudColumnType = symbol;
+                }
+            }
+        }
+    }
+}
