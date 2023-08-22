eba35d220832557b0e9db392563f3c9e42dfed7c
Raghuveer Ch S <raghuveerchs@gmail.com>
2023-08-22 08:03:25 +0530

    Added utility to check if the line intersects hallway.

--- Revit_Automation/Source/Utils/GenericUtils.cs
+++ Revit_Automation/Source/Utils/GenericUtils.cs
@@ -1,4 +1,5 @@
 ﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
 using Revit_Automation.CustomTypes;
 using System;
 using System.Collections.Generic;
@@ -365,9 +366,94 @@ namespace Revit_Automation.Source.Utils
             return dPanelThickness;
         }
 
-        public bool LineIntersectsHallway(XYZ startPt, XYZ endPt)
+        /// <summary>
+        /// Function to get the hatch id of the hallway region type
+        /// Return null if the hallway type is not present
+        /// </summary>
+        public static ElementId GetHallwayHatchId(Document doc)
         {
-            return true;
+            // hatch id for hallway hatch filled region 
+            ElementId hatchId = null;
+
+            // capture hallway hatch element ID
+            var filledRegion = new FilteredElementCollector(doc).OfClass(typeof(FilledRegionType));
+            foreach (var region in filledRegion)
+            {
+                if (region.Name == "Hallway hatch")
+                {
+                    hatchId = region.Id;
+                    break;
+                }
+            }
+
+            return hatchId;
+        }
+
+        /// <summary>
+        /// return hallway region if present otherwise return null
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <returns>hallway hatch as filled region</returns>
+        public static FilledRegion GetHallwayRegion(Document doc)
+        {
+            // collect all the filled regions
+            FilteredElementCollector collector = new FilteredElementCollector(doc, doc.ActiveView.Id);
+            ICollection<Element> filledRegions = collector.OfClass(typeof(FilledRegion)).ToElements();
+
+            FilledRegion hallwayRegion = null;
+
+            // collect hallway region from the filled regions based on hatch id 
+            foreach (var region in filledRegions)
+            {
+                if (region.GetTypeId() == GetHallwayHatchId(doc))
+                {
+                    hallwayRegion = region as FilledRegion;
+                    break;
+                }
+            }
+
+            return hallwayRegion;
+        }
+
+        /// <summary>
+        /// Check if any of the line interesects with the hallway
+        /// </summary>
+        /// <param name="doc">DB Document</param>
+        /// <param name="startPt">start point</param>
+        /// <param name="endPt">end point</param>
+        /// <returns></returns>
+        public static bool LineIntersectsHallway(Document doc,XYZ startPt, XYZ endPt)
+        {
+            //get the hallway region as filled region
+            FilledRegion hallwayRegion = GetHallwayRegion(doc);
+
+            if(hallwayRegion == null)
+            {
+                TaskDialog.Show("Error", "Hallway type or Hallway hatch is not present in this project");
+                return false;
+            }
+
+            IList<CurveLoop> curveLoops = hallwayRegion.GetBoundaries();
+
+            // construct a line from the given points 
+            Line givenLine = Line.CreateBound(startPt, endPt);
+
+            // iterate through all the curveloops
+            foreach (CurveLoop curveLoop in curveLoops)
+            {
+                // iterate through each curve in the curve loop
+                foreach (Curve curve in curveLoop)
+                {
+                    //create a line from the curve, assuming that hallways always have line curves
+                    Line hallwayLine = Line.CreateBound(curve.GetEndPoint(0), curve.GetEndPoint(1));
+
+                    // return true if the lines are intersecting
+                    if (givenLine.Intersect(hallwayLine) == SetComparisonResult.Overlap)
+                        return true;
+                }
+            }
+
+            return false;
         }
     }
 }
